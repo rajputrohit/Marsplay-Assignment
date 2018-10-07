a96eba716ecb66925ef47806b94d9777eb9d0c44
Rohit Rajput <rohit.rajput@ymail.com>
2018-10-07 18:39:21 +0530

    Initial Commit

---
+++ marsplay-assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  marsplay-assignment
+//
+//  Created by Rohit Rajput on 07/10/18.
+//  Copyright © 2018 Rohit Rajput. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
